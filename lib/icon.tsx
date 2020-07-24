de4760cab6c39cb2da31d46db65dadff78577aa5
dandanloveJM <gigi834921748@qq.com>
2020-07-25 01:37:16 +0800

    支持所有事件，并处理classes

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
 import './importIcons'
 import './style.scss'
+import classes from './helper/classes'
 
-interface IconProps {
+interface IconProps extends React.SVGAttributes<SVGElement>{
   name: string,
-  onClick: React.MouseEventHandler<SVGElement>
 }
 // 写成箭头函数方便设置类型
 const Icon: React.FunctionComponent<IconProps> = (props) => {
+  const {className, name, ...restProps} = props;
   return (
-      <svg className='pui-icon' onClick={props.onClick}>
-        <use xlinkHref={`#${props.name}`}/>
+      <svg className={classes('pui-icon', className)}
+           {...restProps}  >
+        <use xlinkHref={`#${name}`}/>
       </svg>
   );
 };
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Icon from './icon';
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from 'react-dom';
+
+
 const fn: React.MouseEventHandler = (e) => {
   console.log(e.target);
 };
 
 
 ReactDOM.render(<div>
-  <Icon name="wechat" onClick={fn} />
-  </div>
-
-  , document.querySelector('#root'))
+    <Icon name="wechat"
+          onClick={fn}
+          onMouseEnter={()=>{console.log('enter')}}
+          onMouseLeave={()=>{console.log('leave')}}
+    />
+  </div>, document.querySelector('#root'));
 
 
 export {Icon};
