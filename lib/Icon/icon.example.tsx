c2fd37d84ca3798a436baa15a522c9c6b9c7c026
dandanloveJM <gigi834921748@qq.com>
2020-07-25 23:59:25 +0800

    开发环境下用example看结果，在webpack.config.prod.js里配置

---
+++ lib/Icon/icon.example.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Icon from './icon';
+
+const IconExample: React.FunctionComponent = () => {
+  return (
+    <div>
+      <Icon name="panda"/>
+      <Icon name="wechat"/>
+    </div>
+  );
+};
+
+export default IconExample;
