5c6a97b3e99325772d3be1f17214927e24145c5b
dandanloveJM <gigi834921748@qq.com>
2020-07-26 20:09:48 +0800

    update

---
+++ doc.sh
@@ -0,0 +1,6 @@
+yarn doc
+git checkout gh-pages
+mv -f doc/* ./
+git add .
+git commit -m "update"
+git push
--- lib/layout/layout.demo.tsx
+++ lib/layout/layout.demo.tsx
@@ -13,15 +13,19 @@ const LayoutDemo: React.FunctionComponent = () => {
     <Fragment >
       <div className="example">
         <Demo code={require('!!raw-loader!./layout.example.1.tsx').default}>
+          <h1>第一个例子</h1>
           <LayoutExample1/>
         </Demo>
         <Demo code={require('!!raw-loader!./layout.example.2.tsx').default}>
+          <h1>第二个例子</h1>
           <LayoutExample2/>
         </Demo>
         <Demo code={require('!!raw-loader!./layout.example.3.tsx').default}>
+          <h1>第三个例子</h1>
           <LayoutExample3/>
         </Demo>
         <Demo code={require('!!raw-loader!./layout.example.4.tsx').default}>
+          <h1>第四个例子</h1>
           <LayoutExample4/>
         </Demo>
       </div>
